7780c30a345ffd1c0d850957fc67773668a13e3d
skorjen <es@cstmarket.ru>
2026-08-03 20:41:14 +0300

    fix: added in auth_vk get_or_create!

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Request
 
-from app.api.dependencies import VkUserIdDep
+from app.api.dependencies import VkUserIdDep, ServiceUserDep
 from app.core.logging import get_logger
 from app.infrastructure.security import create_jwt
 
@@ -10,8 +10,16 @@ auth_rout = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_rout.post("/vk")
-async def auth_vk(vk_id: VkUserIdDep):
-    """Обменивает VK параметры на JWT токен."""
+async def auth_vk(
+        request: Request,
+        user_service: ServiceUserDep,
+):
+    vk_id = int(request.query_params.get("vk_user_id", 0))
+    if not vk_id:
+        raise HTTPException(400, "Missing vk_user_id")
+
+    await user_service.get_or_create_user(vk_id)
+
     token = create_jwt(vk_id)
     return {"access_token": token, "token_type": "bearer"}
 
